6867e61f7aa293ea9e730188cc504a9fc0a59138
Christian Montoya <chrimonlo@gmail.com>
2025-08-29 15:45:36 -0500

    feat(i18n): Create the ui and utils files to use the translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languageList = {
+    es: 'español',
+    en: 'English'
+}
+
+export const labels = {
+    es: {
+        'nav.home': 'Inicio',
+        'nav.experience': 'Experiencia',
+        'nav.about': 'Sobre mí',
+        'nav.projects': 'Proyectos'
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.experience': 'Experience',
+        'nav.about': 'About me',
+        'nav.projects': 'Projects'
+    }
+}
